b0bedb9ad541a03f827b1969001b9c295e2f6d8d
Alek7iy <alek7iy@vk.com>
2026-01-02 12:42:46 +0500

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,9 @@
+public class Numbers{
+    public static void main(String[] args) {
+        int number = 0;
+        while (number <= 25) {
+                System.out.print(number + " ");
+                number++;
+            }
+        }
+    }
