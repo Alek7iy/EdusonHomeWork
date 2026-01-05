b66700462e0af30a13676f84bccea539f52e98df
Alek7iy <alek7iy@vk.com>
2026-01-05 20:15:16 +0500

    Delete Numbers.java

--- Numbers.java
+++
@@ -1,9 +0,0 @@
-public class Numbers{
-    public static void main(String[] args) {
-        int number = 0;
-        while (number <= 25) {
-                System.out.print(number + " ");
-                number++;
-            }
-        }
-    }
