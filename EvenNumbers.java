68734d70ce1e2fcff2578dd5bf11c6293f3759a9
Alek7iy <alek7iy@vk.com>
2026-01-05 20:14:47 +0500

    Delete EvenNumbers.java

--- EvenNumbers.java
+++
@@ -1,9 +0,0 @@
-package Eduson;
-
-public class EvenNumbers {
-        public static void main(String[] args) {
-            for (int i = 12; i <= 20; i += 2) {
-                System.out.print(i + " ");
-            }
-        }
-}
